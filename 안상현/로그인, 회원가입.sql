7344c8f85a696edddcf5e688327f5e4e4a61d7bb
javaspringstudy <java.spring.study@gmail.com>
2024-12-26 10:23:53 +0900

    로그인,회원가입 쿼리문 수정

--- 안상현/로그인, 회원가입.sql
+++ 안상현/로그인, 회원가입.sql
@@ -12,19 +12,20 @@ desc Member;
 INSERT INTO `Member` (password, name, email, nickname) VALUES ('2222','둘리','duly@naver.com','dulygreen');
 
 -- 회원탈퇴 : 회원 정보와 관련된 데이터가 삭제된다.
-DELETE FROM `Member` WHERE email = ? and password = ?;
+DELETE FROM `Member` WHERE email = '?' and password = '?';
 DELETE FROM `Member` WHERE member_id = ?;
 
 -- 로그인 : 아이디와 비밀번호로 로그인 시 로그인 된다.
 UPDATE `Member` SET isLogin = 1 WHERE member_id = ?;
-UPDATE `Member` SET isLogin = 1 WHERE email = ?;
+UPDATE `Member` SET isLogin = 1 WHERE email = '?';
+UPDATE `Member` SET isLogin = 1 WHERE email = '?' and password = '?';
 
 -- 로그아웃
 UPDATE `Member` SET isLogin = 0 WHERE member_id = ?;
-UPDATE `Member` SET isLogin = 0 WHERE email = ?;
+UPDATE `Member` SET isLogin = 0 WHERE email = '?';
 
 -- 회원 정보 수정 : 내 정보를 수정할 수 있다.
-UPDATE `Member` SET password = '?', name = '?', nickname = '?' where email = ?;
+UPDATE `Member` SET password = '?', name = '?', nickname = '?' where email = '?';
 
 #아티스트 
 -- 관리자의 승인을 받으면 아티스트가 될 수 있다.
