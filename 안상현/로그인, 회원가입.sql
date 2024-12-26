760439873d84cc6db207194ab642cdd8d6661a03
javaspringstudy <java.spring.study@gmail.com>
2024-12-26 10:05:47 +0900

    로그인,회원가입 쿼리문 수정

--- 안상현/로그인, 회원가입.sql
+++ 안상현/로그인, 회원가입.sql
@@ -12,20 +12,24 @@ desc Member;
 INSERT INTO `Member` (password, name, email, nickname) VALUES ('2222','둘리','duly@naver.com','dulygreen');
 
 -- 회원탈퇴 : 회원 정보와 관련된 데이터가 삭제된다.
-DELETE FROM `Member` WHERE member_id = ? and password = ?;
+DELETE FROM `Member` WHERE email = ? and password = ?;
+DELETE FROM `Member` WHERE member_id = ?;
 
 -- 로그인 : 아이디와 비밀번호로 로그인 시 로그인 된다.
 UPDATE `Member` SET isLogin = 1 WHERE member_id = ?;
+UPDATE `Member` SET isLogin = 1 WHERE email = ?;
 
 -- 로그아웃
 UPDATE `Member` SET isLogin = 0 WHERE member_id = ?;
+UPDATE `Member` SET isLogin = 0 WHERE email = ?;
 
 -- 회원 정보 수정 : 내 정보를 수정할 수 있다.
-UPDATE `Member` SET password = ?, name = ?, email = ?, nickname = ?;
+UPDATE `Member` SET password = '?', name = '?', nickname = '?' where email = ?;
 
-#아티스트 - 상현
+#아티스트 
 -- 관리자의 승인을 받으면 아티스트가 될 수 있다.
-UPDATE `Member` SET role = '?';
+UPDATE `Member` SET role = '?' where email = '?';
 
+#아래 요구사항은 여기서 처리하기에 약간 애매해서 보류하였습니다.
 -- 아티스트는 노래를 등록할 수 있다.
 -- 등록한 노래를 관리할 수 있다.
