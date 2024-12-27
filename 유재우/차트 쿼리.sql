8c965bb8fb5a2207dd6cbf92bc562875872c5c2e
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2024-12-27 13:54:10 +0900

    [유재우] 연도 차트 쿼리 완성

--- 유재우/차트 쿼리.sql
+++ 유재우/차트 쿼리.sql
@@ -1,49 +1,60 @@
 -- chart_id와 song_id들을 이용해서 song_in_chart에 INSERT
+-- 차트안에 노래가 있으면 지우고 다시 삽입 구현해야 함
 -- call Insert_years_in_chart('2024');
--- 2024년도에 발매된 노래 중에 재생횟수가 높은 순으로 내림차순한 song_id 추출
 DELIMITER $$
 
 CREATE OR REPLACE PROCEDURE Insert_years_in_chart(
-    IN chart_name VARCHAR(10)
+    IN input_chart_name VARCHAR(10)
 )
 BEGIN
-    DECLARE chart_id BIGINT;
+    DECLARE output_chart_id BIGINT(20);
 
     -- chart_name으로 chart_id 조회
-    SELECT chart_id INTO chart_id
-    FROM chart
-    WHERE name = chart_name;
+    SELECT `chart_id` INTO output_chart_id
+    FROM Chart
+    WHERE `name` = input_chart_name;
+    
+    SELECT output_chart_id;
 
     -- song_in_chart에 데이터 삽입
-    INSERT INTO song_in_chart (chart_id, song_id)
+    INSERT INTO Song_In_Chart (chart_id, song_id)
     SELECT 
-        chart_id, 
+        output_chart_id, 
         s.song_id
     FROM 
-        song s
-    INNER JOIN album a ON s.album_id = a.album_id
-    WHERE s.release_date = chart_name AND a.name = chart_name
+        Song s
+    INNER JOIN Album a ON s.album_id = a.album_id
+    WHERE YEAR(a.rel_date) = CAST(input_chart_name AS UNSIGNED)
     ORDER BY s.Streaming_cnt DESC										-- 스트리밍 횟수 내림차순 정렬
 	 LIMIT 100;
 END $$
 
 DELIMITER ;
 
+
+CALL Insert_years_in_chart('2024');
 -- 차트를 부르는 프로시저
 -- char_name: 차트 이름을 뜻 하는 매개변수
--- ex) chart_name -> 신곡차트
 DELIMITER $$
 
-CREATE OR REPLACE PROCEDURE Get_chart (
+CREATE OR REPLACE PROCEDURE Get_Chart (
 	IN chart_name VARCHAR(20)
 )
 BEGIN
-	SELECT *
-	FROM song_in_chart s
-	LEFT OUTER JOIN chart c ON s.chart_id = c.chart_id
-	WHERE s.`name` LIKE chart_name
+	SELECT ROW_NUMBER() OVER(ORDER BY s.Streaming_cnt DESC) AS '순위', 
+		s.`name` AS '곡 제목',
+		s.genre AS '장르',
+		s.album_id AS '앨범',
+		s.Streaming_cnt AS '재생 횟수',
+		s.`length` AS '곡 길이'
+	FROM Song_IStreaming_count_by_membern_Chart sic
+	RIGHT OUTER JOIN Song s ON sic.song_id = s.song_id
+	LEFT OUTER JOIN Chart c ON sic.chart_id = c.chart_id
+	WHERE c.`name` LIKE chart_name
 	ORDER BY Streaming_cnt DESC
 	LIMIT 100;
 END$$
 
 DELIMITER ;
+
+CALL Get_Chart('2024');
