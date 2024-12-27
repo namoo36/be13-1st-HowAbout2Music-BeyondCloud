148af728616e727e897c662c0297bf4333b2b3a8
javaspringstudy <java.spring.study@gmail.com>
2024-12-27 18:42:24 +0900

    로그인, 회원가입 쿼리문 수정완료

--- 안상현/login_member_procedure.sql
+++ 안상현/login_member_procedure.sql
@@ -1,8 +1,5 @@
-create database mockup_beyondcloud default character set utf8;
-use mockup_beyondcloud;
 
-#회원가입 : 회원 정보를 입력하면 회원 정보가 저장된다.
-
+#1. 회원가입 : 회원 정보를 입력하면 회원 정보가 저장된다.
 delimiter $$
 CREATE OR REPLACE PROCEDURE makeMemberProc (
 	IN inputName varchar(20),
@@ -17,20 +14,8 @@ delimiter ;
 
 call makeMemberProc('둘리','2222','duly@naver.com','dulygreen');
 
-#회원탈퇴 : 회원 정보와 관련된 데이터가 삭제된다.
-delimiter $$
-CREATE OR REPLACE PROCEDURE deleteMemberProc(
-	IN inputMemberId bigint(20)
-)
-BEGIN 
-	DELETE FROM Member WHERE member_id = inputMemberId;
-END $$
-delimiter ;
 
-call deleteMemberProc(3);
-
-
--- 로그인 : 아이디와 비밀번호로 로그인 시 로그인 된다.
+#2. 로그인 : 아이디와 비밀번호로 로그인 시 로그인 된다.
 delimiter $$
 CREATE OR REPLACE PROCEDURE loginMemberProc(
 	IN inputEmail varchar(255),
@@ -44,7 +29,7 @@ delimiter ;
 call loginMemberProc('duly@naver.com','2222');
 
 
--- 로그아웃
+#3. 로그아웃 : 564번 회원이 로그아웃 버튼을 클릭하면 로그아웃 된다.
 delimiter $$
 CREATE OR REPLACE PROCEDURE logoutMemberProc(
 	IN inputMemberId bigint(20)
@@ -54,39 +39,46 @@ BEGIN
 END $$
 delimiter ;
 
-call logoutMemberProc(4);
+call logoutMemberProc(564);
 
 
--- 회원 정보 수정 : 내 정보를 수정할 수 있다.
+#4. 회원정보 수정 : 둘리회원의 정보중 닉네임, 비밀번호을 수정할 수 있다.
 delimiter $$
 CREATE OR REPLACE PROCEDURE modifyMemberProc(
-	IN inputName varchar(20),
 	IN inputPassword varchar(20),
 	IN inputNickname varchar(50),
 	IN inputEmail varchar(255)
 )
 BEGIN 
-	UPDATE Member SET name = inputName, password = inputPassword, nickname = inputNickname where email = inputEmail;
+	UPDATE Member SET nickname = inputNickname, password = inputPassword  where email = inputEmail;
 END $$
 delimiter ;
 
-call modifyMemberProc('하니','1234','달려라하니','duly@naver.com');
+call modifyMemberProc('dulyblue','1234','duly@naver.com');
 
 
-#아티스트 
--- 관리자의 승인을 받으면 아티스트가 될 수 있다.
+#5. 아티스트  : 관리자의 승인을 받으면 아티스트가 될 수 있다.
 delimiter $$
 CREATE OR REPLACE PROCEDURE confirmArtistProc(
-	IN inputMemberId varchar(20)
+	IN inputMemberId bigint(20)
 )
 BEGIN 
-	UPDATE Member SET role = 'Arti' where member_id = inputMemberId;
+	UPDATE Member SET role_code = 2 where member_id = inputMemberId;
 END $$
 delimiter ;
 
-call confirmArtistProc(4);
+call confirmArtistProc(564);
 
 
-#아래 요구사항은 여기서 처리하기에 약간 애매해서 보류하였습니다.
--- 아티스트는 노래를 등록할 수 있다.
--- 등록한 노래를 관리할 수 있다.
+#6. 회원탈퇴 : 564 "둘리"회원 정보와 관련된 데이터가 삭제된다.
+delimiter $$
+CREATE OR REPLACE PROCEDURE deleteMemberProc(
+	IN inputMemberId bigint(20)
+)
+BEGIN 
+	DELETE FROM Member WHERE member_id = inputMemberId;
+END $$
+delimiter ;
+
+call deleteMemberProc(564);
+
