f451a72cf4d0c7032b60bb44044744fd7aa00659
javaspringstudy <java.spring.study@gmail.com>
2024-12-29 20:15:20 +0900

    차트 스케줄러 수정

--- 안상현/chart_scheduler.sql
+++ 안상현/chart_scheduler.sql
@@ -1,3 +1,13 @@
+#탑100 차트의 로그를 기록할 테이블
+drop table if exists chart_top100;
+create table if not exists chart_top100(
+	ranking int,
+	name varchar(50),
+	cnt int,
+	exectime date default curdate()
+);
+
+
 #1단계 - 이벤트 스케줄러 상태 활성화
 show variables like '%event_scheduler%';-- 출력값이 OFF라면 스케줄러가 비활성화 상태
 select @@event_scheduler;
@@ -8,32 +18,50 @@ set global event_scheduler = on;
 drop event if exists evt_scheduler_chart100;
 delimiter $$
 create or replace event evt_scheduler_chart100
-on schedule every 1 day
-starts concat(curdate()," 00:00:00")
+on schedule every 1 minute
+starts now() -- concat(curdate()," 00:00:00")
 do
 begin
-	select s.name 노래명, count(*) 재생횟수
+	delete from chart_top100;-- 이전 기록 삭제
+
+	insert into chart_top100 (ranking, name, cnt) 
+	(select ROW_NUMBER() OVER (ORDER BY s.name), s.name 노래명, count(*) 재생횟수
 	from Streaming_count_by_member scbm
 	inner join Song s on scbm.song_id = s.song_id 
-	group by s.song_id
-	order by 2 desc
-	limit 0,100;
+	group by scbm.song_id
+	order by 3 desc
+	limit 0,100);
 end $$
 delimiter ;
 
+select * from chart_top100;
+
+
+
+#장르에 대한 탑100 차트의 로그를 기록할 테이블
+drop table if exists chart_genre_top100;
+create table if not exists chart_genre_top100(
+	ranking int,
+	name varchar(50),
+	cnt int,
+	exectime date default curdate()
+);
 
 #3단계 - 장르에 대한 탑100 차트 (Song 테이블의 재생횟수 Streaming_cnt 컬럼값 기준)
 drop event if exists evt_scheduler_genre100;   
 delimiter $$
 create or replace event evt_scheduler_genre100
-on schedule every 1 day
-starts concat(curdate()," 00:00:00")
+on schedule every 1 minute
+starts now() -- concat(curdate()," 00:00:00")
 do
 begin
-	select name 노래명, sum(Streaming_cnt) 재생횟수	from Song 
+	delete from chart_genre_top100;-- 이전 기록 삭제
+
+	insert into chart_genre_top100 (ranking, name, cnt) 
+	(select ROW_NUMBER() OVER (ORDER BY name), name 노래명, sum(Streaming_cnt) 재생횟수	from Song 
 	group by genre
-	order by 2 desc
-	limit 0,100;
+	order by 3 desc
+	limit 0,100);
 end $$
 delimiter ;   
 
@@ -44,7 +72,7 @@ select * from information_schema.events;
 SELECT * FROM information_schema.EVENTS WHERE EVENT_SCHEMA = 'how2music';
 
 
-
+select * from chart_genre_top100;
 
 
 -- 테스트를 위한 더미데이터
